c7e42c8459641cb54beee0a665c886db22ce2145
mihirniyogi <E1122052@u.nus.edu>
2024-10-27 10:55:47 +0800

    first commit, brought over changes from old repo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { FaRaspberryPi, FaServer } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen border border-white bg-background">
+      <h1 className="mx-4 mt-4 text-xl text-left text-white border border-red-700 sm:text-2xl md:text-3xl lg:text-4xl">
+        Mihir's Home Server
+      </h1>
+    </div>
+  );
+}
+
+export default App;
